bbcc46d5d80995c05c45b6656abe1fc5abc2163f
nskovpin <kolehandro@gmail.com>
2017-06-08 21:47:19 +0300

    feature-2

--- src/main/java/TestDevelop.java
+++ src/main/java/TestDevelop.java
@@ -8,4 +8,8 @@ public class TestDevelop {
     public static void main(String[] args) {
         System.out.println("feature1");
     }
+
+    private void hello(){
+        System.out.println("this is hello");
+    }
 }
