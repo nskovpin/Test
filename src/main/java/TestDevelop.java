4d1de93e8b1fffea516efe1a6045f09531e28000
nskovpin <kolehandro@gmail.com>
2017-06-08 21:46:36 +0300

    feature-1

--- src/main/java/TestDevelop.java
+++ src/main/java/TestDevelop.java
@@ -3,12 +3,13 @@
  */
 public class TestDevelop {
 
-
+    private String test ="test";
     private static final int COUNT = 11;
 
+
     public static void main(String[] args) {
-
-
+        System.out.println("feature1");
         System.out.println("this is develop");
+
     }
 }
